19fe0dbe4ce00a61cc08ff587a36c95ff11b5ade
Piyarul Sekh <your-email@gmail.com>
2026-07-28 09:31:07 +0530

    add this file

--- GUI.py
+++ GUI.py
@@ -1,6 +1,22 @@
 from tkinter import *
 
 window = Tk()
+
+photo = PhotoImage(file='C:\\Users\\USER\\Downloads\\GOAT.png')
+
+label = Label(window,
+              text="Hello",
+              font=('Arial',40,'bold'),
+              fg='#00FF00',
+              bg='black',
+              relief=RAISED,
+              bd=10,
+              padx=20,
+              pady=20,
+              image=photo,
+              compound='bottom')
+label.pack()
+
 window.geometry("420x420")
 window.title("Piyarul first GUI Program")
 
